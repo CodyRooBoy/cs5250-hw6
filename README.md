93ce2f182233f8bb0bd14f5ae67a0fa5b9dd5bd9
Brandon Herrin <brandonherrin@hotmail.com>
2024-10-26 17:39:17 -0600

    Adding command-line arguments

--- README.md
+++ README.md
@@ -3,8 +3,9 @@
 ## Build Instructions
 - The project was built with Python 3.12.3
 - Using `venv`
-    - Run `python3 -m venv env` to create Python virtual environment
+    - Run `python3 -m venv env` to create Python virtual environment. After creating the virtual environment, start the environment by running `source env/bin/activate` (project developed on Ubuntu-24.04)
     - Using `pip3`, install the following packages:
         - `boto3`
 
 ## Run Instructions
+- The project can be run with `python3 consumer.py` followed by the neccessary command-line arguments
--- src/consumer.py
+++ src/consumer.py
@@ -1,1 +1,19 @@
 import boto3
+
+import argparse
+import json
+
+def get_request_s3(request_bucket):
+    s3 = boto3.client('s3')
+    response = s3.list_objects_v2(Bucket=request_bucket)
+
+if __name__ == "__main__":
+    # Parse command-line arguments
+    parser = argparse.ArgumentParser(description="Parse command-line arguments for consumer program")
+
+    parser.add_argument('-rb', '--request-bucket', type=str, help='Name of S3 bucket containing widget requests')
+    parser.add_argument('-rq', '--request-queue', type=str, help='URL of queue containing widget requests')
+    parser.add_argument('-wb', '--widget-bucket', type=str, help='Name of S3 bucket to store widgets')
+    parser.add_argument('-dwt', 'dynamodb-widget-table', type=str, help='DynamoDB table to store widgets')
+
+    args = parser.parse_args()
