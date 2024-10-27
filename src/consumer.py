93024fa13e633a0a14c8d2a6021150395cbeb903
Brandon Herrin <brandonherrin@hotmail.com>
2024-10-26 18:01:50 -0600

    Added request fetching from S3

--- src/consumer.py
+++ src/consumer.py
@@ -3,9 +3,15 @@ import boto3
 import argparse
 import json
 
+
+
 def get_request_s3(request_bucket):
     s3 = boto3.client('s3')
-    response = s3.list_objects_v2(Bucket=request_bucket)
+    requests = s3.list_objects_v2(Bucket=request_bucket)
+    nextKey = min(obj['Key'] for obj in requests['Contents'])
+    nextRequest = s3.get_object(Bucket=request_bucket, Key=nextKey)
+    request = json.loads(nextRequest['Body'].read().decode('utf-8'))
+    return request
 
 if __name__ == "__main__":
     # Parse command-line arguments
@@ -14,6 +20,7 @@ if __name__ == "__main__":
     parser.add_argument('-rb', '--request-bucket', type=str, help='Name of S3 bucket containing widget requests')
     parser.add_argument('-rq', '--request-queue', type=str, help='URL of queue containing widget requests')
     parser.add_argument('-wb', '--widget-bucket', type=str, help='Name of S3 bucket to store widgets')
-    parser.add_argument('-dwt', 'dynamodb-widget-table', type=str, help='DynamoDB table to store widgets')
+    parser.add_argument('-dwt', '--dynamodb-widget-table', type=str, help='DynamoDB table to store widgets')
 
     args = parser.parse_args()
+    print(args)
